a6a857ad1e62bcde3b3904bd62f8ed974f676582
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-10-05 12:11:00 -0500

    Basic routing setup, moved index back to ./

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "./pages/landing-page";
+import WorkspacePage from "./pages/workspace-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/projects",
+    element: <WorkspacePage />,
+  },
+]);
 
 function App() {
-
   return (
     <>
+      <RouterProvider router={router} />
       <h1>Too Simple Analytics</h1>
       <h2>Working on backend before I start building out the frontend</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
