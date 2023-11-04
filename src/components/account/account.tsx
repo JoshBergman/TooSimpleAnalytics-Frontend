251e14da909ba853d20a946af9ef00969ee6da86
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-11-03 20:22:33 -0500

    Manage account basics

--- src/components/account/account.tsx
+++ src/components/account/account.tsx
@@ -1,18 +1,54 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../../store/user/user-context";
 
+import loginStyles from "./styles/login-signup.module.css";
+import styles from "./styles/account.module.css";
+
 const Account = () => {
+  const [isDeletingAccount, setIsDeletingAccount] = useState(false);
   const userCTX = useContext(UserContext).user;
 
   const logOutHandler = () => {
     userCTX.actions.logout();
   };
 
+  const toggleDeletingAccount = () => {
+    setIsDeletingAccount((prevDelAcc) => !prevDelAcc);
+  };
+
+  const deleteAccountHandler = () => {
+    toggleDeletingAccount();
+  };
+
   return (
-    <p>
-      Logged in and manage account here{" "}
-      <button onClick={logOutHandler}>Log Out</button>
-    </p>
+    <div className={loginStyles.form}>
+      <h1 className={loginStyles.heading}>Manage Account</h1>
+
+      <button
+        style={{ marginBottom: "20px" }}
+        className={loginStyles.actionButton}
+        onClick={logOutHandler}
+      >
+        Log Out
+      </button>
+      {isDeletingAccount ? (
+        <div className={styles.delAccDiv}>
+          <button className={styles.halfButton} onClick={deleteAccountHandler}>
+            Confirm Delete Account
+          </button>
+          <button className={styles.halfButton} onClick={toggleDeletingAccount}>
+            Cancel
+          </button>
+        </div>
+      ) : (
+        <button
+          className={loginStyles.actionButton}
+          onClick={toggleDeletingAccount}
+        >
+          Delete Account
+        </button>
+      )}
+    </div>
   );
 };
 
