77701ecbda55246c52843e5162e74dadc3601a88
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-11-04 18:57:35 -0500

    Styling and functionality of connect project logic

--- src/components/projects/single-project/connect-project-methods.tsx
+++ src/components/projects/single-project/connect-project-methods.tsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 
 import styles from "./styles/connect-project-method.module.css";
-import { get_connect_methods } from "../../../store/static-info";
+import { get_connect_methods, staticInfo } from "../../../store/static-info";
 import { UserContext } from "../../../store/user/user-context";
 
 interface IConnectProjectMethodsProps {
@@ -9,26 +9,52 @@ interface IConnectProjectMethodsProps {
 }
 
 const ConnectProjectMethods = ({ projName }: IConnectProjectMethodsProps) => {
-  const methods: string[] = get_connect_methods("", "", true);
-  const [methodShowing, setMethodShowing] = useState("recommended");
+  const methods: string[] = get_connect_methods("", "", true) as string[];
+  const [methodShowing, setMethodShowing] = useState(methods[0]);
   const userCTX = useContext(UserContext).user;
-  const projLink = `https://tsa-real-765f9ae13226.herokuapp.com/analytic/view/${userCTX.projectsID}-${projName}`;
+  const projLink = `${staticInfo.uri}/analytic/view/${userCTX.projectsID}-${projName}`;
+
+  const getTabs = () => {
+    const tabs: React.ReactNode[] = methods.map((method) => {
+      const currTab = (
+        <button
+          className={styles.tab}
+          key={method + "-tab"}
+          style={
+            method === methodShowing
+              ? {
+                  backgroundColor: "var(--text-default)",
+                  color: "var(--background-1)",
+                  borderColor: "var(--brand-1)",
+                }
+              : {}
+          }
+          onClick={() => {
+            setMethodShowing(method);
+          }}
+        >
+          {method}
+        </button>
+      );
+      return currTab;
+    });
+
+    return tabs;
+  };
 
   return (
     <div className={styles.container}>
-      <div className={styles.tabsContainer}>
-        <button
-          className={styles.tab}
-          onClick={() => {
-            setMethodShowing("recommended");
-            //dynamically render these using keys from connect_methods
-          }}
-        >
-          Recommended
-        </button>
-      </div>
+      <h5 className={styles.subText}>
+        A view is counted each time a GET request is sent to this projects
+        endpoint.
+      </h5>
+      <div className={styles.tabsContainer}>{getTabs()}</div>
       <div className={styles.methodContainer}>
-        {get_connect_methods(methodShowing, projLink)}
+        <pre className={styles.snippetPre}>
+          <code className={styles.codeSnippet}>
+            {get_connect_methods(methodShowing, projLink) as string}
+          </code>
+        </pre>
       </div>
     </div>
   );
---
+++ src/store/static-info.tsx
@@ -0,0 +1,57 @@
+export const staticInfo = {
+  uri: "https://tsa-real-765f9ae13226.herokuapp.com",
+};
+
+export const get_connect_methods = (
+  connect_method: string,
+  projLink: string,
+  keys?: boolean
+) => {
+  const connect_methods: { [index: string]: React.ReactNode } = {
+    "Recommended (JS)": `
+  export const addSiteView = () => {
+  const milliseconds_Until_Next_View_Counts = 86400000; //1 day in milliseconds
+  const viewUrl = "${projLink}";
+
+  const saveDate = () => {
+    const thisDateAsString = new Date().getTime() + "";
+    localStorage.setItem("viewed", thisDateAsString);
+  };
+
+  const retrieveLastViewDate = () => {
+    const storedValue = localStorage.getItem("viewed");
+    if (storedValue == null) {
+      return 0;
+    }
+    const storedDate = parseInt(storedValue);
+    return storedDate;
+  };
+
+  const getNextViewTime = () => {
+    const lastViewTime = retrieveLastViewDate();
+    const timeTillNextView = lastViewTime + milliseconds_Until_Next_View_Counts;
+    return timeTillNextView;
+  };
+
+  const nextViewTime = getNextViewTime();
+  const currentTime = new Date().getTime();
+
+  if (currentTime >= nextViewTime) {
+    try {
+      //fetches view, then saves the time the view was added to local storage
+      fetch(viewUrl);
+      saveDate();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+};`,
+    "Raw Link": `${projLink}`,
+  };
+
+  if (keys) {
+    return Object.keys(connect_methods);
+  }
+
+  return connect_methods[connect_method];
+};
