40c282b730be22c632154c4d90ddd6422d8e4b27
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-11-05 16:35:44 -0600

    Basic styling of notificatons

--- src/components/UI/notifications/notifications.tsx
+++ src/components/UI/notifications/notifications.tsx
@@ -1,13 +1,26 @@
 // Notifications are the informational pop-ups for errors, success msgs, and/or additional information
 // currently this is mounted to the header and may require some refactoring to be re-usable
 import { useContext } from "react";
-import { AppStateContext } from "../../../store/app-state/app-state-context";
+import { AiFillInfoCircle, AiFillCheckCircle } from "react-icons/ai";
+import { BiSolidErrorAlt } from "react-icons/bi";
 
+import { AppStateContext } from "../../../store/app-state/app-state-context";
 import styles from "./styles/notifications.module.css";
 
 const Notifications = () => {
   const notifications = useContext(AppStateContext).appState.notifications;
   //notifications === [type of notification ("info", "error", "success"), msg][]
+
+  const getIcon = (notificationType: string) => {
+    switch (notificationType) {
+      case "info":
+        return <AiFillInfoCircle className={styles.icon} />;
+      case "error":
+        return <BiSolidErrorAlt className={styles.icon} />;
+      case "success":
+        return <AiFillCheckCircle className={styles.icon} />;
+    }
+  };
 
   const renderNotifications = (): React.ReactNode[] => {
     const renderedNotifications: React.ReactNode[] = [];
@@ -21,6 +34,7 @@ const Notifications = () => {
           className={styles[`notification-${thisNotificationType}`]}
           key={thisNotificationID}
         >
+          {getIcon(thisNotificationType)}
           {thisNotificationMsg}
         </div>
       );
